78c30a5816bb361f328462df94007883f6884f46
unknown <umar.ovie@gmail.com>
2023-01-14 13:09:17 +0700

    ?

--- server/db.js
+++ server/db.js
@@ -34,14 +34,14 @@ function generateConfig(config, withDatabase = true) {
   const result = {};
   result.host = config.host || '127.0.0.1';
   result.user = config.user || 'root';
-  // result.password = config.password || 'root';
-  result.password = config.password || 'secret';
+  result.password = config.password || 'root';
+  // result.password = config.password || 'secret';
   if (withDatabase) {
     result.database = config.database || 'uas';
   }
   result.connectionLimit = config.connectionLimit || 10;
-  // result.port = config.port || 3306;
-  result.port = config.port || 3333;
+  result.port = config.port || 3306;
+  // result.port = config.port || 3333;
   result.multipleStatements = true;
   result.supportBigNumbers = true;
   return result;
